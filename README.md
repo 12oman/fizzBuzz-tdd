a9fec359daf2941a94549787a9040c0cc48218e4
12oman <romanmitch@gmail.com>
2022-09-06 12:00:24 +1200

    test setup working

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # fizzBuzz-tdd
+
+when given an array of numbers, return fizz for multiples of 3 and buzz for multiples of 5 and fizzbuzz for multiples of both 3 and 5
+
---
+++ fizzbuzz.test.js
@@ -0,0 +1,7 @@
+// import testing lib
+
+test('test setup working', () => {
+    expect(true).toBeTruthy()
+  })
+
+// when given the number 3 fizzbuzz should return fizz
